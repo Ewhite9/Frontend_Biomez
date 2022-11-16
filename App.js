21e0cfeadf2f89073b2236da42a18620db00cbb7
sgnahouidavid1 <81520639+sgnahouidavid1@users.noreply.github.com>
2022-11-16 16:22:16 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from "./Components/Navbar";
+
+export default function App()
+{
+  return(
+    <div className="Navbar">
+      <Navbar />
+    <body>
+      
+    </body>
+  
+    </div>
+  );  
+}
